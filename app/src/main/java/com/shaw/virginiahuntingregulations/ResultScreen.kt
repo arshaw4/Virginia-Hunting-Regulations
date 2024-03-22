916e1b67b3f3da5abf4a5fb4de29d7a05adae163
arshaw4 <alex04shaw@gmail.com>
2024-03-22 19:01:08 -0400

    added result screen

--- app/src/main/java/com/shaw/virginiahuntingregulations/ResultScreen.kt
+++ app/src/main/java/com/shaw/virginiahuntingregulations/ResultScreen.kt
@@ -1,29 +1,25 @@
 package com.shaw.virginiahuntingregulations
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import java.util.Calendar
 
-class SpeciesInput : AppCompatActivity() {
+class ResultScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_species_input)
+        setContentView(R.layout.activity_result_screen)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
         val calendar = intent.getStringExtra("date")
-        findViewById<Button>(R.id.submitSpeciesCountButton).setOnClickListener{
-            val intent = Intent(this, ResultScreen::class.java)
-            intent.putExtra("date", calendar)
-            startActivity(intent)
-        }
+    }
+    private fun dateInRange(startRange: Calendar, endRange: Calendar, checkDate: Calendar): Boolean{
+        return checkDate.after(startRange) && checkDate.before(endRange)
     }
 }
