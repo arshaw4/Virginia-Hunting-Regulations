4e746e48a8e9ed0cbf97e85297d29db281c60dbd
arshaw4 <alex04shaw@gmail.com>
2024-04-09 00:52:35 -0400

    added passing inputs to results screen

--- app/src/main/java/com/shaw/virginiahuntingregulations/ResultScreen.kt
+++ app/src/main/java/com/shaw/virginiahuntingregulations/ResultScreen.kt
@@ -1,8 +1,10 @@
 package com.shaw.virginiahuntingregulations
 
 import android.os.Bundle
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.SwitchCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import java.util.Calendar
@@ -18,8 +20,18 @@ class ResultScreen : AppCompatActivity() {
             insets
         }
         val calendar = intent.getStringExtra("date")
+        val tealValue = intent.getIntExtra("tealValue", -1)
+        val mergansersValue = intent.getIntExtra("mergansersValue",-1)
+
+
+        val huntZoneSwitchValue = intent.getBooleanExtra("huntZoneSwitchValue", false)
+        val i95ZoneSwitchValue = intent.getBooleanExtra("i95ZoneSwitchValue", false)
+        //tealResult.text = tealResult(calendar,findViewById<EditText>(R.id.tealInputValue).text)
     }
     private fun dateInRange(startRange: Calendar, endRange: Calendar, checkDate: Calendar): Boolean{
         return checkDate.after(startRange) && checkDate.before(endRange)
+    }
+    private fun tealResults(calendar: Calendar, tealInput: String, i95SwitchResult: Boolean){
+
     }
 }
--- app/src/main/java/com/shaw/virginiahuntingregulations/SpeciesInput.kt
+++ app/src/main/java/com/shaw/virginiahuntingregulations/SpeciesInput.kt
@@ -3,11 +3,12 @@ package com.shaw.virginiahuntingregulations
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.SwitchCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import java.util.Calendar
 
 class SpeciesInput : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,6 +24,26 @@ class SpeciesInput : AppCompatActivity() {
         findViewById<Button>(R.id.submitSpeciesCountButton).setOnClickListener{
             val intent = Intent(this, ResultScreen::class.java)
             intent.putExtra("date", calendar)
+            intent.putExtra("tealValue", findViewById<EditText>(R.id.tealInputValue).text.toString().toInt())
+            intent.putExtra("mergansersValue", findViewById<EditText>(R.id.mergansersInputValue).text.toString().toInt())
+            intent.putExtra("mallardDrakeValue", findViewById<EditText>(R.id.mallardDrakeInputValue).text.toString().toInt())
+            intent.putExtra("mallardHenValue", findViewById<EditText>(R.id.mallardHenInputValue).text.toString().toInt())
+            intent.putExtra("woodDuckValue", findViewById<EditText>(R.id.woodDuckInputValue).text.toString().toInt())
+            intent.putExtra("blackDuckValue", findViewById<EditText>(R.id.blackDuckInputValue).text.toString().toInt())
+            intent.putExtra("scaupValue", findViewById<EditText>(R.id.scaupInputValue).text.toString().toInt())
+            intent.putExtra("redheadValue", findViewById<EditText>(R.id.redheadInputValue).text.toString().toInt())
+            intent.putExtra("canvasbackValue", findViewById<EditText>(R.id.canvasbackInputValue).text.toString().toInt())
+            intent.putExtra("pintailValue", findViewById<EditText>(R.id.pintailInputValue).text.toString().toInt())
+            intent.putExtra("mottledDuckValue", findViewById<EditText>(R.id.mottledDuckInputValue).text.toString().toInt())
+            intent.putExtra("fulvousWhistlingValue", findViewById<EditText>(R.id.fulvousWhistlingInputValue).text.toString().toInt())
+            intent.putExtra("cootValue", findViewById<EditText>(R.id.cootInputValue).text.toString().toInt())
+            intent.putExtra("geeseValue", findViewById<EditText>(R.id.geeseInputValue).text.toString().toInt())
+            intent.putExtra("scoterValue", findViewById<EditText>(R.id.scoterInputValue).text.toString().toInt())
+            intent.putExtra("eiderDrakeValue", findViewById<EditText>(R.id.eiderDrakeInputValue).text.toString().toInt())
+            intent.putExtra("eiderHenValue", findViewById<EditText>(R.id.eiderHenInputValue).text.toString().toInt())
+            intent.putExtra("longTailValue", findViewById<EditText>(R.id.longTailInputValue).text.toString().toInt())
+            intent.putExtra("huntZoneSwitchValue", findViewById<SwitchCompat>(R.id.huntZoneSwitch).isChecked)
+            intent.putExtra("i95ZoneSwitchValue", findViewById<SwitchCompat>(R.id.i95ZoneSwitch).isChecked)
             startActivity(intent)
         }
     }
